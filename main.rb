b1b8bca39668c33e0065ae213e63b711430f7e12
Cornelius <bartelsjacobcorn@gmail.com>
2024-07-01 10:07:40 -0400

    Fix: Add rubocop to bundle and run it

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def caesar_cipher(string, shift_factor)
   result_array = string.chars.map do |char|
     if char.match?(/[[:alpha:]]/)
@@ -11,16 +13,16 @@ def caesar_cipher(string, shift_factor)
 end
 
 def shift_letter(char, shift_factor)
-  high_code = (char.ord > 90) ? 122 : 90
+  high_code = char.ord > 90 ? 122 : 90
   new_char_num = char.ord + (shift_factor % 26)
   new_char_num = (new_char_num - 26) unless new_char_num <= high_code
   new_char_num.chr
 end
 
-puts "Please enter your string"
+puts 'Please enter your string'
 string = gets.chomp
 
-puts "Please enter your shift factor"
+puts 'Please enter your shift factor'
 shift_factor = gets.chomp.to_i
 
 puts caesar_cipher(string, shift_factor)
